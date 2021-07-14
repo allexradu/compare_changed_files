0c21faf90effcb82095ee065bf24107cc91d1277
allexraducs <alex.radu@conentspeed.ro>
2021-07-14 11:07:57 +0300

    adding the right print statements

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ for file in source_files:
     search_file = file.replace(r'c:\wamp64\singlecoreb2b', r'c:\wamp64\blade\singlecoreb2b')
     if search_file not in destination_files:
         if search_file.find('node_modules') == -1:
-            print(file)
+            # print(file)
             files.append(file)
 
     else:
@@ -26,27 +26,25 @@ for file in source_files:
             if file.find('node_modules') == -1:
                 files.append(file)
                 original_files.append(search_file)
-                print(file)
+                # print(file)
 
 for file in files:
     file_name = file.split('\\')[-1]
     file_path = '\\'.join(file.split('\\')[3:-1])
     output_file_path = os.path.join(r'c:\wamp64\output', file_path)
-    print(output_file_path)
     os.makedirs(output_file_path, exist_ok=True)
     shutil.copyfile(file, os.path.join(output_file_path, file_name))
 
-    print(f'Copied {file} > {os.path.join(output_file_path, file_name)}')
+    print(f'Separating Modified File: {file} > {os.path.join(output_file_path, file_name)}')
 
 for original_file in original_files:
     file_name = original_file.split('\\')[-1]
     file_path = '\\'.join(original_file.split('\\')[4:-1])
     output_file_path = os.path.join(r'c:\wamp64\original', file_path)
-    print(output_file_path)
     os.makedirs(output_file_path, exist_ok=True)
     shutil.copyfile(original_file, os.path.join(output_file_path, file_name))
 
-    print(f'Copied {original_file} > {os.path.join(output_file_path, file_name)}')
+    print(f'Backing up: {original_file} > {os.path.join(output_file_path, file_name)}')
 
 # with open(json_path, 'w') as outfile:
 #     json.dump(files, outfile, indent= 4)
